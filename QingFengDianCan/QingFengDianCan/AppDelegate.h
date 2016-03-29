2e28dd9065a9a18cba17d9620ab35ff105d56d89
Sherrly <1625861520@qq.com>
2016-03-29 16:28:48 +0800

    init项目

---
+++ QingFengDianCan/QingFengDianCan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QingFengDianCan
+//
+//  Created by iOS on 16/3/29.
+//  Copyright © 2016年 zrh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QingFengDianCan/QingFengDianCanTests/QingFengDianCanTests.m
@@ -0,0 +1,39 @@
+//
+//  QingFengDianCanTests.m
+//  QingFengDianCanTests
+//
+//  Created by iOS on 16/3/29.
+//  Copyright © 2016年 zrh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QingFengDianCanTests : XCTestCase
+
+@end
+
+@implementation QingFengDianCanTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
